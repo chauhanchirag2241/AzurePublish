418c4cce14983cbf8eb68bfe15816dd4948d1145
chauhanchirag2241 <coderchirag2272@gmail.com>
2024-10-15 14:53:41 +0530

    first commit

---
+++ azurewebapplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace azurewebapplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ azurewebapplication/Models/coursemodel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace azurewebapplication.Models;
+
+public class coursemodel
+{
+     public int courseid{get; set;}   
+    public string? coursename{get;set;}
+    public string? rating {get;set;}
+}
